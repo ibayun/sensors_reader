6cbe1adba830d5ba0cdc2eb0b655c0a0c92f3ee5
ibayun <i.korniyenko@list.ru>
2025-01-25 13:44:48 +0000

    fixed the first iteration

--- src/sample_script.py
+++ src/sample_script.py
@@ -1,49 +1,74 @@
+import os 
+
+from contextlib import contextmanager
+from datetime import datetime
+
 import serial
 import struct
+import bluetooth
+import json
 
-ser = serial.Serial('/dev/rfcomm0', 9600, timeout=1)
+from dotenv import load_dotenv
 
-
-def read_sensor_values(raw):
-    # Reading bytes (2 bytes per int value sensor, 4 for float)
-    data = raw[0] + raw[1] if len(raw) > 1 else raw[0]
-
-    # Preapre 2 first bytes from  A0 pin (moisture)
-    moisture = (data[1] << 8) | data[0]
-
-    # Prepare 4 bytes as float from A? pin (DS18B20)
-    temperature = struct.unpack('<f', bytes(data)[2:6])[0]
-
-    # CO2 sensor extract the data
-    co2_sensor_value = (data[7] << 8) | data[6]
-    # co2_sensor_value = 0
-
-    # Gray scale sensor
-    gray_scale = (data[9] << 8) | data[8]
-
-    pressure = struct.unpack('<f', bytes(data)[10:14])[0]
-    altimeter = (data[15] << 8) | data[14]
-    return moisture, temperature, co2_sensor_value, gray_scale, pressure, altimeter
+load_dotenv()
 
 
-def retrieve_data():
-    ser.write(b'1')
-    data = ""
+@contextmanager
+def socketcontext(*args, **kwargs):
+    bluetooth_adress = os.environ.get("BLUETOOTH_ADRESS")
+    print(f"Adress - {bluetooth_adress}\n")
+
+    if not bluetooth_adress:
+        raise ValueError("bluetooth_adress is not in .env file")
+    print(f"Trying to connect - {datetime.now()}\n")
+    sensor_address = bluetooth_adress
+    socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    socket.connect((sensor_address, 1))
+    print("Connected\n")
+    
     try:
-        data = ser.readlines()
-    except Exception as exc:
-        print(exc)
+        yield socket
+    finally:
+        socket.close()
+        print("DisConnected\n")
+
+
+def read_the_data_from_socked(socket):
+    data = ''
+    piece = b''
+    end_of_string = b'\r\n'
+    while end_of_string not in piece:
+        piece = socket.recv(1048)
+        data += str(piece, encoding='ascii')
+    return data
+
+
+def updated_parser():
+    with socketcontext() as sock:
+        print("Start READing...\n")
+        sock.send(b'1')
+        parsed_data = read_the_data_from_socked(sock)
+        try:
+            data = json.loads(parsed_data)
+            print('dumpet data\n\n')
+            print(f"{data} - dict ---")
+        except json.JSONDecodeError as e:
+            print(f"cautghr the next error - {e}")
+    
     return data
 
 
 def run_sensor_collect():
-    moisture = temperature = co2_sensor_value = gray_scale = ""
+    data = {}
     try:
-        moisture, temperature, co2_sensor_value, gray_scale, *args = read_sensor_values(retrieve_data())
+        data = updated_parser()
     except KeyboardInterrupt:
         print("Program was interruted by user")
-    return moisture, temperature, co2_sensor_value, gray_scale
+    return data
 
 
 if __name__ == "__main__":
-    print(run_sensor_collect())
+    while True:
+        import time
+        print(run_sensor_collect())
+        time.sleep(2)
